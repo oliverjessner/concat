46bcb4fbe8f3876294558a3e764eb52eb89ba58e
oliverjessner <oliver.jessner@gmail.com>
2020-05-17 18:40:34 +0200

    trying to fixx this mess

--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@
 'use strict'
 const concat = require('ganescha-concat');
 const fs = require('fs');
-const [, output, ...files] = process.argv;
+const [,, output, ...files] = process.argv;
 
 console.log(files);console.log(output);
 concat(files, output).then(() => console.log('ganescha-concat done')).catch(console.error);
